7930b89d2458fd9355df6312b97ea65c53900dde
Dvlp <biola.gold@gmail.com>
2019-07-06 16:31:48 +0100

    [Prolifik] added customer area

--- Imovie.App/Startup.cs
+++ Imovie.App/Startup.cs
@@ -13,14 +13,11 @@ namespace Imovie.App
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -29,8 +26,11 @@ namespace Imovie.App
             }
             app.UseStaticFiles();
 
+
             app.UseMvc((routes) => 
             {
+                routes.MapRoute("areaRoute", "{area=exists}/{controller=Dashboard}/{action=index}/{id?}");
+
                 routes.MapRoute("default", "{controller=home}/{action=index}/{id?}");
             });
         }
